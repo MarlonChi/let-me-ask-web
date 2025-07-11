c3816d334416200424348c43cbe23c0ad3226975
MarlonChi <marlonchiodelli@hotmail.com>
2025-07-10 21:53:09 -0300

    feat: let me ask

--- src/app.tsx
+++ src/app.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from "react-router-dom";
 import { CreateRoom } from "./pages/create-room";
 import { Room } from "./pages/room";
+import { RecordRoomAudio } from "./pages/record-room-audio";
 
 export function App() {
   return (
     <Routes>
       <Route index element={<CreateRoom />} />
       <Route element={<Room />} path="/rooms/:roomId" />
+      <Route element={<RecordRoomAudio />} path="/room/:roomId/audio" />
     </Routes>
   );
 }
